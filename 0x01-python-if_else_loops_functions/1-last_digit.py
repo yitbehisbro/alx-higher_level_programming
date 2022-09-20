727031a0a1e1cdeee7622f0bffbf76ae37f3105d
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-09-20 16:16:15 +0000

    Assigns a random signed number to the variable number each time it is executed

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number >= 0:
+    num = number % 10
+else:
+    num = number % -10
+
+print("Last digit of {} is {}".format(number, num), end='')
+
+if num > 5:
+    print(" and is greater than 5")
+elif num == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")
