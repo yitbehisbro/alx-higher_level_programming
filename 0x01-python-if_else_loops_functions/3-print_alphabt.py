803dc5d2290e62badb869b554cdf8db1b3ba5931
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-09-20 10:54:43 +0000

    Print lowercase letters except "e" and "q"

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if i == 101:
-        continue;
+        continue
     if i == 113:
-        continue;
+        continue
     print("{:c}".format(i), end="")
