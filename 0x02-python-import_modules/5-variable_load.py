85f01e9f01b1e1c8d0e748661f1d3b58462dae21
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-09-22 06:26:49 -0700

    Imports the variable a from the file variable_load_5.py and prints its value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from variable_load_5 import a as num
+from variable_load_5 import a
 import sys
 if __name__ == "__main__":
-    print("{}".format(num))
+        print("{}".format(a))
