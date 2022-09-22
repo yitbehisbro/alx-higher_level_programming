5af685b01f30374ef56727cbc8b3250a4ed2a6e0
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-09-22 07:31:43 -0700

    Calculation

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>", end="\n")
+        exit(1)
+    else:
+        for i in range(1, len(sys.argv)):
+            if sys.argv[2] == "+":
+                result = add(int(sys.argv[1]), int(sys.argv[3]))
+            elif sys.argv[2] == "-":
+                result = sub(int(sys.argv[1]), int(sys.argv[3]))
+            elif sys.argv[2] == "*":
+                result = mul(int(sys.argv[1]), int(sys.argv[3]))
+            elif sys.argv[2] == "/":
+                result = div(int(sys.argv[1]), int(sys.argv[3]))
+            else:
+                print(f"Unknown operator. Available operators: +, -, * and /")
+                exit(1)
+        print("{} {} {} = {}\
+            ".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), result))
