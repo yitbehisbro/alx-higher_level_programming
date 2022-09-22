d950c12ce78641f98f4fb0c1b1af3de2a8287741
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-22 17:40:24 +0300

    Updates

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,17 +7,16 @@ if __name__ == "__main__":
         print(f"Usage: ./100-my_calculator.py <a> <operator> <b>", end="\n")
         exit(1)
     else:
-        for i in range(1, len(sys.argv)):
-            if sys.argv[2] == "+":
-                result = add(int(sys.argv[1]), int(sys.argv[3]))
-            elif sys.argv[2] == "-":
-                result = sub(int(sys.argv[1]), int(sys.argv[3]))
-            elif sys.argv[2] == "*":
-                result = mul(int(sys.argv[1]), int(sys.argv[3]))
-            elif sys.argv[2] == "/":
-                result = div(int(sys.argv[1]), int(sys.argv[3]))
-            else:
-                print(f"Unknown operator. Available operators: +, -, * and /")
-                exit(1)
-        print("{} {} {} = {}\
-            ".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), result))
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == "+":
+            print("{} {} {} = {}".format(a, sys.argv[2], add(a, b))
+        elif sys.argv[2] == "-":
+            print("{} {} {} = {}".format(a, sys.argv[2], sub(a, b))
+        elif sys.argv[2] == "*":
+            print("{} {} {} = {}".format(a, sys.argv[2], mul(a, b))
+        elif sys.argv[2] == "/":
+            print("{} {} {} = {}".format(a, sys.argv[2], div(a, b))
+        else:
+            print(f"Unknown operator. Available operators: +, -, * and /")
+            exit(1)
