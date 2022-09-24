34233436ed65eed80e6e5ad090022aab5fb50243
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-24 16:12:36 +0300

    Updates

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx <= len(my_list) and idx >= 0:
-        index_at = my_list[idx]
-        return index_at
+        return my_list[idx]
     else:
         return None
