ef8b9ee837a454e5ebe6ae2c04d0f1ed797a3d49
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-24 15:51:40 +0300

    Updates

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     if my_list is not None and len(my_list) >= 0:
         for integer in my_list):
-            print("{:d}".format(integer))
+            print("{integer:d}".format(integer))
