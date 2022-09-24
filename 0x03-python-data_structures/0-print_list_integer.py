37a416afa8abecee7036cf5545b86b95e45da646
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-24 15:38:02 +0300

    Error fixes

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{}".format(integer))
+    for integer in range(len(my_list)):
+        print("{}".format(my_list[integer]))
