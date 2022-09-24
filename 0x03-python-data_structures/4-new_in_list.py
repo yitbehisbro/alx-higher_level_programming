d11d2a174d6a81e7aac5daff74a77ec2a45eaf24
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-24 16:39:19 +0300

    Updates

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cpy_list = my_list.copy()
-    cpy_list[idx] = element
-    return cpy_list
+    if idx < 0:
+        return my_list.copy()
+    elif idx >= len(my_list):
+        return my_list.copy()
+    else:
+        cpy_list = my_list.copy()
+        cpy_list[idx] = element
+        return cpy_list
