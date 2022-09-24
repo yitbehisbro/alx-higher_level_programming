b0b393a30d7ecb32a0bf9dab78fbd3829c28ca38
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-24 16:43:46 +0300

    Updates

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[i])):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
                 if j < len(matrix[i]) - 1:
                     print("{:d}".format(matrix[i][j]), end=" ")
                     else:
