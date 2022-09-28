abd59329ca91e80d53013a869abc03cd9084f21d
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-28 19:30:36 +0300

    Typo fixes

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,6 @@
 def complex_delete(a_dictionary, value):
 	lists = list(a_dictionary.keys())
     for i in lists:
-        if j == a_dictionary.get(i):
+        if value == a_dictionary.get(i):
             del a_dictionary[i]
     return a_dictionary
