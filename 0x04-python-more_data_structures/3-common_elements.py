d8d373ade4615379f464ff74ef3ff5aa15f18d01
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-28 13:53:45 +0300

    Updates

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/us/bin/python3
 def common_elements(set_1, set_2):
-    intersection = []
-    if set_1 is not None and set_2 is not None:
-        intersection = set_1 & set_2
-        return intersection
+    return set_1 & set_2
