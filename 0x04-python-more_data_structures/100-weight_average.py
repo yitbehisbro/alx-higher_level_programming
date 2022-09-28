9b084802fecfca0c98224678b779f546d570ff83
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-28 19:17:05 +0300

    Error fixes

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    n, d = 0, 0
+    if not my_list:
         return 0
     else:
-        n = 0
-        d = 0
         for t in my_list:
             n += t[0] * t[1]
             d += t[1]
