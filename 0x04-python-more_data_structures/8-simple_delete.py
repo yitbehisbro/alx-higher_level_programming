1094b364842ff72ade888b07ec9beaac5295be79
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-28 15:44:06 +0300

    Error fixes

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary is not None:
-        if key in a_dictionary:
-            rm = a_dictionary.get(key)
-            del rm
+    if a_dictionary is not None and key is not None:
+        if a_dictionary.get(key) is not None:
+            del a_dictionary[key]
             return a_dictionary
         else:
             return a_dictionary
