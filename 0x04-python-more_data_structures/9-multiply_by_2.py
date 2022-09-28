e51256257c8c9eb0d76e81fb4e84e6d437a6d53e
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-28 17:38:50 +0300

    Error fixes

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     if a_dictionary is not None:
-        for i in list(a_dictionary.copy()).keys():
-            a_dictionary.copy()[i] *= 2
-        return a_dictionary.copy()
+        n_dic = a_dictionary.copy()
+        for i in list(n_dic.keys()):
+            n_dic[i] *= 2
+        return n_dic
