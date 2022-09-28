094d6f67a211ee913ae45fc0fa0b805b0ae9dc65
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-28 13:55:36 +0300

    Error fixes

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,6 @@
-#!/us/bin/python3
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return set_1 & set_2
+    intersection = []
+    if set_1 is not None and set_2 is not None:
+        intersection = set_1 & set_2
+        return intersection
