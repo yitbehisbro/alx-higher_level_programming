baba77c6d78dbb65fed23e79e965f3adff4de748
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-09-28 05:11:58 -0700

    Deletes a key in a dictionary.

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is not None and key is not None:
-        element = a_dictionary.get(key)
-        del element
-        return a_dictionary
+        if key in a_dictionary:
+            rm = a_dictionary.get(key)
+            del rm
+            return a_dictionary
+        else:
+            return a_dictionary
