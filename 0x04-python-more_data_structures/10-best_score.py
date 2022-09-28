8a8929c0a79f9a050d3354207618227c48eda0f3
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-28 18:14:06 +0300

    Updates

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        max_v = max(list(a_dictionary.keys()))
-        return max_v
+        return max(a_dictionary.keys())
     else:
         return None
