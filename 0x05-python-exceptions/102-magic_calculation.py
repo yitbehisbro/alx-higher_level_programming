84db445f8f239b72dc0ffb4c6b013c5eded64906
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-10 12:28:21 +0300

    Updates

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    magic = 0
+    for j in range(1, 3):
+        try:
+            if (j > a):
+                raise Exception("Too far")
+            else:
+                magic += (a ** b) / j
+        except:
+            magic = b + a
+            break
+    return magic
