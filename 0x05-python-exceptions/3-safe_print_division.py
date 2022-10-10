047b158e7c5cd7accd0e9ab563d192b3baf83208
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-10 03:21:39 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         div = a / b
-    except ValueError:
+    except (ValueError, TypeError, ZeroDivisionError):
         div = None
     finally:
         print("Inside result: {}".format(div))
