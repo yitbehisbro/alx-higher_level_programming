fecff3a33a90a2e761a9efe11c03ebaacdb288cd
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-10 03:14:24 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except TypeError:
+    except (RuntimeError, TypeError, NameError):
         return False
     else:
         return True
