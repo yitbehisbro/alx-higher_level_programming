db53fc22908de0c33ee5656c5cfb047a895e5d49
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-11 01:06:19 -0700

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """This class do calculation for square. """
+    def __init__(self, size):
+        """This is the intialization builtin fuction.
+
+        Args:
+            size (int): the size of squares
+        """
+        self.__size = size
