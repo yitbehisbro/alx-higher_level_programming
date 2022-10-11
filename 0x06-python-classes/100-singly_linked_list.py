6447f5b04bfadbe591ccf9b8e929de593f8de766
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-11 04:11:42 -0700

    0x06. Python - Classes and Objects

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,32 +1,62 @@
 #!/usr/bin/python3
+""" A module that operates like single linked list.
+"""
+
+
 class Node:
+    """ A class that defines a node of a singly linked list.
+    """
     def __init__(self, data, next_node=None):
+        """ Built in method for intiallization.
+        Args:
+            data (int): integer data to be added to node
+            next_node: points to the next node
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """ Returns or retrive value for setted
+        Returns: the data
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """ Sets the data's value.
+        Args:
+            value (int): values to be setted.
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """ Method that retrive the next_node.
+        Returns: the next_node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """ Sets the next_node's value.
+        Args:
+            next_node (int): next_node to be setted.
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """ A class that defines a singly linked list
+    """
     def __str__(self):
+        """ Built in str method
+        Returns: the output
+        """
         output = ""
         point = self.__head
 
@@ -39,9 +69,15 @@ class SinglyLinkedList:
         return output
 
     def __init__(self):
+        """ Builtin method for intallization
+        """
         self.__head = None
 
     def sorted_insert(self, value):
+        """ Sorts the value
+        Args:
+           value (int): integer value
+        """
         point = self.__head
 
         while point is not None:
