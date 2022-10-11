f7337a2714aed4f1c313d57f49ae0f698d8a9415
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-11 02:45:13 -0700

    0x06. Python - Classes and Objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    """This class do calculation for square.
+    """ This class do calculation for square.
     """
     def __init__(self, size=0):
-        """This is the intialization builtin fuction.
+        """ This is the intialization builtin fuction.
         Args:
             size (int): the size of squares
         Raises:
@@ -18,20 +18,20 @@ class Square:
             raise TypeError("size must be an integer")
 
     def area(self):
-        """Returns the size of the area.
+        """ Returns the size of the area.
         """
         return self.__size * self.__size
 
     @property
     def size(self):
-        """This is the function that retrieve size.
+        """ This is the function that retrieve size.
         Returns: the retrived size
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """This is the function that set size.
+        """ This is the function that set size.
         Args:
             value (int): the size of squares
         Raises:
