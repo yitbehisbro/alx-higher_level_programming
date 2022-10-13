986596bfa662b48731103c476cdca954bd24ff3a
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-13 01:46:32 -0700

    Pycodestyle fixes and pydoc

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+""" The module that adds two integers.
+"""
+
+
 def add_integer(a, b=98):
-	if type(a) not in [int, float]:
-		raise TypeError("a must be an integer")
-	if type(b) not in [int, float]:
-		raise TypeError("b must be an integer")
-	if type(a) not in [int]:
-		a = int(a)
-	if type(b) not in [int]:
-		b = int(b)
-	return a + b
+    """ Method that accepts integer or float.
+    But,
+        Returns: the casted integer only.
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    if type(a) not in [int]:
+        a = int(a)
+    if type(b) not in [int]:
+        b = int(b)
+    return a + b
