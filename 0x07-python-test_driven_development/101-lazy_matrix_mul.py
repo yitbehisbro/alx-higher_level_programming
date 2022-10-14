4205a79dc09ded63cec12c0213fcd66167dda97a
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-14 02:12:58 -0700

    Multiplies 2 matrices by using the module NumPy

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,8 +2,17 @@
 """ Lazy matrix multiplication """
 
 
-import numpy as n
+import numpy
+
+
 def lazy_matrix_mul(m_a, m_b):
-    """ Multiplies 2 matrices by using the module NumPy """
-    mul = n.dot(m_a, m_b)
-    return mul
+    """ Multiplies 2 matrices by using the module NumPy
+        Args:
+            m_a (list): first matrix
+            m_b (list): second matrix
+        Returns: matrix multplication
+        Raises:
+            TypeError: if m_a and m_b are not a list or equal
+            ValueError: if m_a and m_b can’t be multiplied or empty
+    """
+    return numpy.matmul(m_a, m_b)
