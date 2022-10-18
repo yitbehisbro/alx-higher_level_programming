cfbb0d865156734ed797248af1ac064ad3e8bf76
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-18 01:55:10 -0700

    0x09. Python - Everything is object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(lists=[]):
+    lists += ["BestSchool"]
+    return ", ".join(lists)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Locked class example """
+
+
+class LockedClass:
+    """ Locked class """
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Instance intallization """
+        pass
