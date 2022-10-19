42e45dc79df8b207ed86b6caed23cfdeae3804b8
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-19 11:14:19 +0300

    Updates

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,4 +16,7 @@ def add_integer(a, b=98):
         a = int(a)
     if type(b) not in [int]:
         b = int(b)
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
     return a + b
