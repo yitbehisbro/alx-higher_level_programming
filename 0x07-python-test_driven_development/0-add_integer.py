2b7f07c448ccbed8082533cf936c64ba4f2d7e3d
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-19 11:46:39 +0300

    Updates

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,15 +8,9 @@ def add_integer(a, b=98):
     But,
         Returns: the casted integer only.
     """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return 89
+
     return int(a) + int(b)
