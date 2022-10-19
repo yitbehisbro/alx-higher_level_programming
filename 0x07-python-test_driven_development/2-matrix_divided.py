55f671713964523042f216fa3e62cae5d323d49b
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-19 10:39:09 +0300

    Error fixes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -17,23 +17,32 @@ def matrix_divided(matrix, div):
             TypeError: matrix must be a matrix
                 (list of lists) of integers/floats
     """
-    flag = None
-    n = []
-    m_copy = matrix.copy()
-    message = "Each row of the matrix must have the same size"
-    message2 = "matrix must be a matrix (list of lists) of integers/floats"
-
-    if len(m_copy[0]) != len(m_copy[1]):
-        raise TypeError(message)
-    for row in m_copy:
-        for col in range(len(row)):
-            if type(row[col]) not in [int, float]:
-                flag = 1
-    if type(div) not in [int, float]:
+    if not type(div) in (int, float):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if flag == 1:
-        raise TypeError(message2)
-    n = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
-    return n
+
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(msg_type)
+
+    len_e = 0
+    msg_size = "Each row of the matrix must have the same size"
+
+    for elems in matrix:
+        if not elems or not isinstance(elems, list):
+            raise TypeError(msg_type)
+
+        if len_e != 0 and len(elems) != len_e:
+            raise TypeError(msg_size)
+
+        for num in elems:
+            if not type(num) in (int, float):
+                raise TypeError(msg_type)
+
+        len_e = len(elems)
+
+    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (m)
