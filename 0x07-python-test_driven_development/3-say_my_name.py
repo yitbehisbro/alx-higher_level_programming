ff9ab38b562a3bce8b1012affe8d3da92bfaa667
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-19 11:06:04 +0300

    Updates

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,9 +7,9 @@ def say_my_name(first_name, last_name=""):
     """ Prints My name is <first name> <last name>
     """
 
-    if type(first_name) not in [str] or first_name is None:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    elif type(last_name) not in [str] or last_name is None:
+    elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
     else:
-        print("My name is {} {}".format(first_name, last_name))
+        print("My name is {:s} {:s}".format(first_name, last_name))
