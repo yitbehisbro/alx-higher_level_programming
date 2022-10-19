67d72bac30cfed208a1fbd3cb2d22f1a52c405c7
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-19 11:51:11 +0300

    Updates

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -11,8 +11,6 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     elif type(size) in [float] and size < 0:
         raise TypeError("size must be an integer")
-    elif size == 0:
-        print()
     else:
         for i in range(size):
             for j in range(size):
