e30510fc233a621807dba4b449cd95f5260fd94b
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-24 05:45:17 -0700

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Geometry module """
+
+
+class BaseGeometry:
+    """ A Base Geometry class """
+
+    def area(self):
+        """ Method for area calculation """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates integer
+            Args:
+                name (str): name
+                value (int): integer
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
+
+    def __init__(self, width, height):
+        """ Initalization class """
+        self.__width = width
+        self.__height = height
+        self.integer_validator(width, height)
+
+    def integer_validator(self, width, height):
+        """ Validates integer
+            Args:
+                width (int): width of the rectangle
+                height (int): height of the rectangle
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if width <= 0:
+            raise ValueError("{} must be greater than 0".format(width))
+        if width <= 0:
+            raise ValueError("{} must be greater than 0".format(height))
