728c17ddc7147ba64d4eab986cb4e3505ea99169
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-24 02:52:13 -0700

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Exact same object """
+
+
+def is_same_class(obj, a_class):
+    """ Returns True if the object is exactly an instance
+    of the specified class ; otherwise False """
+    return True if isinstance(obj, a_class) else False
