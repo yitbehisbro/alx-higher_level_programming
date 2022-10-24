90bdbbb0f8e5fd7514f5623239b8cb5b7d275010
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-24 03:43:44 -0700

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Geometry module """
+
+
+class BaseGeometry:
+    """ A Base Geometry class """
+    def area(self):
+        """ Method for area calculation """
+        raise Exception("area() is not implemented")
