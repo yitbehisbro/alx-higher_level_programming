4a2a910bf6c077ca7527c1065522d601ad09bcd2
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-24 04:08:07 -0700

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,11 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Validates integer """
+        """ Validates integer
+            Args:
+                name (str): name
+                value (int): integer
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
