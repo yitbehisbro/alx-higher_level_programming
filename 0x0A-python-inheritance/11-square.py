0d1751f9c39eed0ad62d4c7fb7fbf9f7a0c32711
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-24 06:27:44 -0700

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+
+    def __init__(self, size):
+        """ Initalization class """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """ Area of square """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ String representation """
+        text = "".join("[Square] {}/{}".format(self.__size, self.__size))
+        return text
