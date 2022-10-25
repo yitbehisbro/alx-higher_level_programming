2dbda22bddee5f252444f3755395e27a35943560
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-25 03:58:31 -0700

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Load, add, save file module"""
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+import os.path as path
+import sys
+obj = []
+if path.exists("add_item.json"):
+    obj = load_from_json_file("add_item.json")
+for i in sys.argv[1:]:
+    obj.append(i)
+save_to_json_file(obj, "add_item.json")
