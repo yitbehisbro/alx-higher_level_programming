2a7c552426699e8d57e425e016db7a7072ba0ebb
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-25 04:09:25 -0700

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Class to JSON module"""
+
+
+def class_to_json(obj):
+    """ Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for
+    JSON serialization of an object
+    """
+
+    return obj.__dict__.copy() if hasattr(obj, "__dict__") else None
