cc3e5aed7a66554958963cf0252389fe5dc11f32
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-25 05:26:56 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,7 +6,6 @@ def append_write(filename="", text=""):
     """ Appends a string at the end of a text file (UTF8)
     and returns the number of characters added
     """
-    counter = 0
     with open(filename, mode='a', encoding="utf-8") as files:
         files.write(text)
     return len(text)
