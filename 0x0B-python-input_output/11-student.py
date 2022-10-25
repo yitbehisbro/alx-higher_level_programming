a43cc88bee294b1b8fb1e151f438842a84c1bcaa
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-25 04:38:36 -0700

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Student to JSON module"""
+
+
+class Student:
+    """ Students class """
+    def __init__(self, first_name, last_name, age):
+        """ Initalization """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns the dictionary description """
+        dict_l = {}
+        if type(attrs) is list:
+            for obj in attrs:
+                if type(obj) is not str:
+                    return self.__dict__.copy()
+            for i in range(len(attrs)):
+                for j in self.__dict__.copy():
+                    if attrs[i] == j:
+                        dict_l[j] = self.__dict__.copy()[j]
+            return dict_l
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for i in json:
+            self.__dict__[i] = json[i]
