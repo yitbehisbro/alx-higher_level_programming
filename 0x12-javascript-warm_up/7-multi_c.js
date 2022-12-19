5e5d25073fd459cc12a4406ca63656520746918c
= <=>
2022-12-19 03:14:53 -0800

    0x12. JavaScript - War
    m up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const args = parseInt(process.argv);
 let string;
+
 if (isNaN(args[0])) {
   string = 'Missing number of occurrences';
   console.log(string);
 } else {
   string = 'C is fun';
-  console.log(string);
   for (let i = 0; i <= args[0]; i++) {
     console.log(string);
   }
