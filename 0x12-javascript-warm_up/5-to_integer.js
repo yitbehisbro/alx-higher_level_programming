ce842e2780dcbebd639aed5b5c7d24ac5ae644ab
= <=>
2022-12-19 02:48:31 -0800

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
-
-if (args[1]) {
-  if (isNaN(parseInt(args[1], 10))) {
-    console.log('Not a number');
-  } else {
-    console.log('My number: '+args[1]);
+let num;
+num = parseInt(args[2]);
+if (isNaN(num)) {
+  num = 'Not a number';
 } else {
-  console.log('Not a number');
+  num = ('My number: ' + num);
 }
+console.log(num);
