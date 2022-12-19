1ad8dfd00fbb170b7336b838c5b88aae64f18c99
= <=>
2022-12-19 02:54:31 -0800

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const i = 0; i <= 2; i++) {
+const strings = ['C is fun', 'Python is cool',
+                 'JavaScript is amazing'];
+let i = 0;
+for (i = 0; i <= 2; i++) {
   console.log(strings[i]);
 }
