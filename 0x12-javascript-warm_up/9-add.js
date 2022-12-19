fdfe5e24016cac6e9a1facd214b3a65b7f654444
= <=>
2022-12-19 03:58:00 -0800

    0x12. JavaScript - War
    m up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-function add(a, b) { return a + b }
-const args = process.argv;
-if (args.slice(2).length < 3) {
-  console.log('NaN');
-} else {
-  if (isNaN(parseInt(args[1])) && isNaN(parseInt(args[2]))) {
-    console.log('NaN');
-  }
-  console.log(add(parseInt(args[1]), parseInt(args[2])));
-}
+function add(a, b) { console.log(parseInt(a) + parseInt(b)) }
+add(process.argv[2], process.argv[3]);
