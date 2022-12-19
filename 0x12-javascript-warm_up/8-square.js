6b20a975fd8ef7d169053def8250c023439a8d48
= <=>
2022-12-19 03:40:29 -0800

    0x12. JavaScript - War
    m up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 let args = parseInt(process.argv[2]);
-
 if (Number.isNaN(args)) {
   console.log('Missing size');
 } else {
-  for (let i = 0, s; i < args; i++) {
-    s = '';
+  for (let i = 0, string; i < args; i++) {
+    string = '';
     for (let j = 0; j < args; j++) {
-      s += 'X';
+      string += 'X';
     }
-    console.log(s);
+    console.log(string);
   }
 }
