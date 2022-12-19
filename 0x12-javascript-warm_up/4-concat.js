925f17c83e57e07bb07418283afdb3eb7a7f3b80
= <=>
2022-12-19 02:35:35 -0800

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-
-if (args.length <= 3) {
-  console.log(args[1]+' is '+args[2]);
+if (args[0] === undefined && args[1] === undefined) {
+  console.log('undefined is undefined');
+} else if (args[0] !== undefined && args[1] !== undefined) {
+  console.log(args[0] + ' is ' + args[1]);
+} else if (args[0] !== undefined && args[1] === undefined) {
+  console.log(args[0] + ' is undefined');
+} else if (args[0] === undefined && args[1] !== undefined) {
+  console.log('undefined is ' + args[1]);
 }
