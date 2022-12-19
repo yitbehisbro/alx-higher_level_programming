4d5a8b1baa9a89b01ae98a2baa1dd85707d1ab34
= <=>
2022-12-19 03:21:57 -0800

    0x12. JavaScript - War
    m up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (isNaN(parseInt(args[1]))) {
+if (isNaN(parseInt(args[1], 10))) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < args[1]; i++) {
