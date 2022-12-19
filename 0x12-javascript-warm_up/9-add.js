554c12219d475501eea2ff06963c05180830545d
= <=>
2022-12-19 03:56:26 -0800

    0x12. JavaScript - War
    m up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,5 +4,8 @@ const args = process.argv;
 if (args.slice(2).length < 3) {
   console.log('NaN');
 } else {
+  if (isNaN(parseInt(args[1])) && isNaN(parseInt(args[2]))) {
+    console.log('NaN');
+  }
   console.log(add(parseInt(args[1]), parseInt(args[2])));
 }
