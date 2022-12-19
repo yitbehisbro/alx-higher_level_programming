b603fd606dac027139f40e546cb07ddb06188228
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-19 12:52:55 +0300

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+
+if (args.slice(2).length > 1) {
+	console.log('Arguments found');
+} else if (args.slice(2).length === 1) {
+	console.log('Argument found');
+} else {
+	console.log('No argument');
+}
