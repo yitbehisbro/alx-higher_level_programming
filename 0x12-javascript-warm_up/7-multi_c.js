27b914da5c366d9f78e5319359910bd5e604980a
= <=>
2022-12-19 03:17:23 -0800

    0x12. JavaScript - War
    m up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const args = parseInt(process.argv);
-let string;
 
-if (isNaN(args[0])) {
+if (isNaN(args[1])) {
   string = 'Missing number of occurrences';
   console.log(string);
 } else {
   string = 'C is fun';
-  for (let i = 0; i <= args[0]; i++) {
+  for (let i = 0; i <= args[1]; i++) {
     console.log(string);
   }
 }
