2e2c41f73903ab4299f5129824faadbc87e188fb
= <=>
2022-12-19 02:23:23 -0800

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if (args.slice(2).length === 0) {
-  console.log('No argument');
+if (args.0) {
+  console.log(args.0);
 } else {
-  console.log(args[0]);
+  console.log('No argument');
 }
