8d162d7d92f281da0b75b0d78fc134c801acf9bc
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-19 15:19:50 +0300

    Updates

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,7 @@
 #!/usr/bin/node
-
-let biggest = 0;
-let i;
-const arrayNumbers = [];
-
-for (i = 2; i < process.argv.length; i++) {
-  if (Number.isNaN(parseInt(process.argv[i])) === false) {
-    arrayNumbers[i - 2] = parseInt(process.argv[i]);
-  }
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number).slice(2).sort((a, b) => a - b).reverse();
+  console.log(args[1]);
 }
-
-if (arrayNumbers.length > 1) {
-  biggest = Math.max.apply(null, arrayNumbers);
-  i = arrayNumbers.indexOf(biggest);
-  arrayNumbers[i] = -Infinity;
-  biggest = Math.max.apply(null, arrayNumbers);
-}
-
-console.log(biggest);
