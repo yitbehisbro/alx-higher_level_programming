463856f4eecefbf1afd0ba91cdc76f658155c0b2
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-20 12:13:50 +0300

    Prints the rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  width;
+  height;
+
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+    if (w === 0 || h === 0) {
+      new Rectangle();
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let string = '';
+      for (let j = 0; j < this.width; j++) {
+        string += 'X';
+      }
+      console.log(string);
+    }
+  }
+}
