707d4f679cee863e1f7ef0e4aa3cfa115265492c
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-20 15:29:02 +0300

    Updates

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let reverseList = new Array();
+  for (let i = list.length - 1; i >= 0; i--) {
+    reverseList = push(list[i]);
+  }
+  return reverseList;
+}
