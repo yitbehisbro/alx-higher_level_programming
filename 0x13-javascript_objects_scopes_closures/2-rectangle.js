0f70395bbe134e6fc1cfcc0b0f41661ff34be57a
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-20 11:53:19 +0300

    Adds condition for constructor arguments

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+  width;
+  height;
+
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+    if (w === 0 || h === 0) {
+      new Rectangle();
+    }
+  }
+}
