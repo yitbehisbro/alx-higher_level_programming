925279499dac86a4a564efb84840a77dcf15f376
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-20 16:52:03 +0300

    Semi standard error fixed

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-exports.esrever = function(list) {
-  let reverseList = [];
+exports.esrever = function (list) {
+  const reverseList = [];
   const len = list.length - 1;
 
   for (let i = len; i >= 0; i--) {
     reverseList.push(list[i]);
   }
   return reverseList;
-}
+};
