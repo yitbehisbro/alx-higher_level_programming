156a2765406ffe48f5d5ad83af4c04bc18a5193d
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-20 15:30:14 +0300

    Updates

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 exports.esrever = function (list) {
   let reverseList = new Array();
   for (let i = list.length - 1; i >= 0; i--) {
-    reverseList = push(list[i]);
+    reverseList.push(list[i]);
   }
   return reverseList;
 }
