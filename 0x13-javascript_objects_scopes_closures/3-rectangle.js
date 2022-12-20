ebb12c5d40f6d593bb70ece4282b39d7e81a457c
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-20 12:33:23 +0300

    Semi standard code fixes

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       let string = '';
