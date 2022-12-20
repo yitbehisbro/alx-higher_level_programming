65461920e78809ba57311043f8c0b75bbc39463d
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-20 14:13:38 +0300

    Updates

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
-  for (const elem of list) {
-    if (elem === searchElement) {
+  for (let elem = 0; elem < list.length; elem++) {
+    if (list[elem] === searchElement) {
       counter++;
     }
   }
