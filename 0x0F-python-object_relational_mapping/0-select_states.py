dc0033fe618b0c27fa34416282f2399242c1379f
= <=>
2022-12-21 00:42:13 -0800

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,12 @@ import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    conn = MySQLdb.connect(
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
     conn_cursor = conn.cursor()
 
     conn_cursor.execute("SELECT * FROM states ORDER BY id ASC")
