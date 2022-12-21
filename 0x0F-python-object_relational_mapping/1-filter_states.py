aa6cba390bb75b6e9d8ebf87aa115a8326470eb6
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-21 12:06:31 +0300

    List country with a name start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
+
+    conn_cursor = conn.cursor()
+    conn_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = conn_cursor.fetchall()
+    for row in query_rows:
+        print(row)
