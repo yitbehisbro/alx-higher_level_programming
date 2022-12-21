fbfb4c8f8bf4bcf1e6c12fbc62198f174e688bcf
= <=>
2022-12-21 01:13:17 -0800

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
             )
 
     conn_cursor = conn.cursor()
-    conn_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    sql_cmd = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    conn_cursor.execute(sql_cmd)
     query_rows = conn_cursor.fetchall()
     for row in query_rows:
         print(row)
