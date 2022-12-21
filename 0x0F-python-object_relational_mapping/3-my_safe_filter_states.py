2526c98bebab2337a35e76f72cf531a96c586742
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-21 12:48:07 +0300

    File created

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
+
+    conn_cursor = conn.cursor()
+    query = "SELECT * FROM states WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = %(name)s"
+    conn_cursor.execute(query, {"name": sys.argv[4]})
+    query_rows = conn_cursor.fetchall()
+    for row in query_rows:
+        print(row)
