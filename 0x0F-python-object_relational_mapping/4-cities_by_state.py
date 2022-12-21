b6505bb910409c6a31dff9936a7f27b282f31585
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-21 12:57:41 +0300

    File created

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
+
+    conn_cursor = conn.cursor()
+    sql_cmd = "SELECT * FROM cities ORDER BY id ASC"
+    conn_cursor.execute(sql_cmd)
+    query_rows = conn_cursor.fetchall()
+    for row in query_rows:
+        print(row)
