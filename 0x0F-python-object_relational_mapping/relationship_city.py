7b2ba1b8fe4822085ae95f2c9585ae405ae40c06
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-12-22 15:06:37 +0300

    content added

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ SQLAlchemy class """
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from relationship_state import Base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
 
-
-class State(Base):
+class City(Base):
     """ Defination for the database """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, nullable=False, primary_key=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
