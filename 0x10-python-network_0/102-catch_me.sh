4c3fe4b126b241648d9d31adc2f0dd6b6264ead5
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-01-16 17:05:29 +0300

    Updates

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin: You got me!"
+curl -sLX PUT -d "user_id=98" -H "Origin: You got me!" 0.0.0.0:5000/catch_me
