b88693a60125126db5b49ec70a1454826ff6f6e5
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-16 16:19:36 +0300

    0x10. Python - Network #0

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL passed as the first argument,
 # and displays the body of the response
-curl -X POST "$1" -d "@$2"
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
