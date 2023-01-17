51a5cc6d0bb6414fdfe6289492e25a3e79bf0da4
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 06:31:32 -0800

    Search API with requests

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter. """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    param = {'q': ''}
+
+    try:
+        if sys.argv[1]:
+            param['q'] = sys.argv[1]
+    except:
+        pass
+
+    try:
+        result = requests.post("http://0.0.0.0:5000/search_user", data=param)
+        my_json = result.json()
+        if my_json:
+            print("[{}] {}".format(my_json.get('id'), my_json.get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
