ff2d62165d29822fa1118bd0615225082ef30502
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 14:53:14 +0300

    The 'urllib.error.HTTPError' error handler

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Sends a request to the URL and displays the body of the
+response (decoded in utf-8). """
+from urllib.error import HTTPError
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            body = response.read('utf-8')
+    except HTTPError as error:
+        if error.code:
+            print("Error code: {}".format(error.code))
