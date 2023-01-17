d70622e225ad1a40b7feeee7bfe8d83e2d011542
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 15:52:02 +0300

    POST an email with 'requests.post'

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" POST an email with the 'requests' """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    result = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(result)
