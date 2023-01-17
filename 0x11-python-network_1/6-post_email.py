097f7952279d0806ec0b8a1b6463f8d760b487c5
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 05:03:44 -0800

    POST email using 'request.post' and displays the body of responses

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,6 @@ import sys
 
 
 if __name__ == "__main__":
-    result = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-    print(result)
+    requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    result = requests.get(sys.argv[1])
+    print(result.content)
