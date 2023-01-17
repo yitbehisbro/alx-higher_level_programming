33de730a799ccb6e5b0bbe7fc47a170c879245b4
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 07:30:03 -0800

    Log in to the Github through API using 'requests'

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    result = requests.get("https://api.github.com/user", auth=(usr, pwd))
+    if result.status_code == 200:
+        r = requests.get("https://api.github.com/user", auth=(usr, pwd)).json()
+        print("{}".format(r["id"]))
+    else:
+        print("None")
