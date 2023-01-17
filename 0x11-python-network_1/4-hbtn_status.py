8dabde6f255eaedf1f3d60caed12f44a121796f4
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 15:12:43 +0300

    Fetches the URL data using 'requests'

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Fetches the URL with requests """
+import requests
+
+
+if __name__ == "__main__":
+    page = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(page.content)))
+    print("\t- content: {}".format(page.content.decode('utf-8')))
