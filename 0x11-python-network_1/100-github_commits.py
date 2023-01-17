cf5d0da0a6ffd3acd99ef302c26569858dbfb599
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 08:17:07 -0800

    Prints the recent top 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Lists 10 commits (from the most recent to oldest) of the
+repository rails by the user rails
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    user = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits"
+
+    result = requests.get(url.format(user, repo))
+
+    if result.status_code == 200:
+        r = result.json()
+        for i in range(10):
+            print("{}: {}".format(r[i]['sha'], r[i]['author']['login']))
+    else:
+        print("None")
