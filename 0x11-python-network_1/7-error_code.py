744448db11eed39a483cd412a2029b53c5f0cbd8
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 06:01:25 -0800

    Status code handler with 'requests.status_code'

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     result = requests.get(sys.argv[1])
     if result.status_code >= 400:
         print("Error code: {}".format(result.status_code))
-    print(result.content.decode('utf-8'))
+    else:
+        print(result.content.decode('utf-8'))
