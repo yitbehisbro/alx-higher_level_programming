81323b0d53ace3089c76404413ba41fd90d2f606
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-17 10:11:21 +0300

    0x10. Python - Network #0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sX PUT -L -d --header "origin: You got me!" -X POST 0.0.0.0:5000/catch_me
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:School"
