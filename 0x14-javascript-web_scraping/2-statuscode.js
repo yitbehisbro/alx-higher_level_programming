5592bd3f8c23e6e97570d3f4bc5ef67f053ddb85
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-09 14:17:33 +0000

    Semistandard fixes

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-request = require('request');
-request.get(process.argv[2])
+require('request').get(process.argv[2])
   .on('response', function (response) {
     console.log('code: ' + response.statusCode);
   });
