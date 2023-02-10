dc84582e9ecba1ca940532eb9450123ca245cec7
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-02-10 13:53:54 +0300

    Error fixed

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const idNum = process.argv[2];
 
-request.get(
-  url,
-  async (err, res, body) => {
-    if (err) console.error(err);
-    else {
-      const film = JSON.parse(body);
-
-      for (const character of film.characters) {
-        request.get(character, (err, res, body) => {
-          if (err) console.error(err);
-          const char = JSON.parse(body);
-          console.log(char.name);
-        });
-      }
+request('http://swapi.co/api/films/${idNum}/', function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const characters = resp.characters;
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
     }
   }
-);
+});
