b17a0a6ee6755468c0f9a841492f17957a619f43
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-20 13:14:09 +0300

    Add class to the element using jQuery API

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $("DIV#red_header").click(function() {
-    $("header").addClass("red");
+$(document).ready(function () {
+  $('DIV#red_header').click(function () {
+    $('header').addClass('red');
   });
 });
