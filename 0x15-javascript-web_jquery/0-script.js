296794a042b14e5ba01e29150912391598f65741
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-20 12:21:18 +0300

    Change color using tagname of the elements

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,2 @@
 const changeColor = document.querySelector("header");
-changeColor.style.color = "#FF0000";
+  changeColor.style.color = "#FF0000";
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+const changeColor = document.getElementsByTagName("header");
+for (let i = 0; i < changeColor.length; i++) {
+  changeColor[i].style.color = "#FF0000";
+}
